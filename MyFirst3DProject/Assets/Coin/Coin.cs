b9f90ffc0b2dc9f2f240741a6ac5e4fcb15aed2b
joanby <joan-by@hotmail.com>
2018-06-26 18:18:07 +0200

    Primer proyecto completado

---
+++ MyFirst3DProject/Assets/Coin/Coin.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * NAMESPACE
+ * 
+ */
+using System.Collections;
+using UnityEngine;
+
+
+/*
+    CLASS
+*/
+public class Coin : MonoBehaviour 
+{
+
+
+    //PROPIEDADES Y MÉTODOS
+    //Variable global y estática (una para todas las monedas)
+    public static int coinsCount = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Coin.coinsCount++;
+        Debug.Log("El juego ha comenzado y ahora hay " + Coin.coinsCount + " monedas");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //Debug.Log("Estamos en el Update");
+	}
+
+    /*
+     * Método que se llama automáticamente cuando
+     * otro collider entra en contacto con el 
+     * que tiene este script (en particular la moneda)
+     */
+    private void OnTriggerEnter(Collider otherCollider)
+	{
+
+        if (otherCollider.CompareTag("Player") == true)
+        {
+            //Como el jugador choca contra la moneda, ahora hay una menos
+            Coin.coinsCount--;
+
+            Debug.Log("Hemos recogido la moneda y ahora hay " + Coin.coinsCount + " monedas");
+
+            //En el caso de que el contador de monedas llegue a cero, hemos recogido todo!
+            if (Coin.coinsCount == 0){
+                Debug.Log("El juego ha terminado");
+
+                GameObject gameManager = GameObject.Find("GameManager");
+
+                Destroy(gameManager);
+
+                GameObject[] fireworksSystem = GameObject.FindGameObjectsWithTag("Fireworks");
+
+                foreach(GameObject fireworks in fireworksSystem){
+                    fireworks.GetComponent<ParticleSystem>().Play();
+                }
+
+
+            }
+
+            Destroy(gameObject);
+        }
+	}
+
+}
---
+++ MyFirst3DProject/Assets/Manager/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+
+    public float maxTime = 60f;
+
+    private float countdown = 0f;
+
+	// Use this for initialization
+	void Start () {
+        countdown = maxTime;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //El deltaTime es el tiempo en segundos que ha pasado desde que se
+        //renderizó en pantalla el último frame anterior.
+
+        countdown -= Time.deltaTime; // countdown = countdown - Time.deltaTime;
+
+        Debug.Log("Cuenta atrás :" + countdown);
+        if(countdown <= 0){
+            Debug.Log("Te has quedado sin tiempo... HAS PERDIDO!!!!");
+
+            Coin.coinsCount = 0;
+
+            SceneManager.LoadScene("MainScene");
+        }
+
+
+	}
+}
