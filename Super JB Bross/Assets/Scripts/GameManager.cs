ee7532b4429e35ec2852eb4ac12597f2aa5f04a9
joanby <joan-by@hotmail.com>
2018-07-02 09:37:15 +0200

    Game Manager

---
+++ Super JB Bross/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Posibles estados del videojuego
+public enum GameState{
+    menu,
+    inGame, 
+    gameOver
+}
+
+public class GameManager : MonoBehaviour {
+
+    //Variable para saber en qué estado del juego nos encontramos ahora mismo
+    //Al inicio, queremos que empiece en el menúb principal
+    public GameState currentGameState = GameState.menu;
+
+	private void Start()
+	{
+        StartGame();
+	}
+
+	//Método encargado de iniciar el juego
+	public void StartGame(){
+        SetGameState(GameState.inGame);
+    }
+
+    //Método que se llamará cuando el jugador muera
+    public void GameOver(){
+        SetGameState(GameState.gameOver);
+    }
+
+    //Método para volver al menú principal cuando el usuario lo quiera hacer
+    public void BackToMenu(){
+        SetGameState(GameState.menu);
+    }
+
+   
+    //Método encargado de cambiar el estado del juego
+    void SetGameState(GameState newGameState){
+
+        if (newGameState == GameState.menu)
+        {
+            //Hay que preparar la escena de Unity para mostrar el menú
+
+        }
+        else if (newGameState == GameState.inGame)
+        {
+            //Hay que preparar la escena de Unity para jugar
+
+        }
+        else if (newGameState == GameState.gameOver)
+        {
+            //Hay que preparar la escena de Unity para el Game Over
+
+        }
+
+        //Asignamos el estado de juego actual al que nos ha llegado por parámetro
+        this.currentGameState = newGameState;
+    }
+
+}
