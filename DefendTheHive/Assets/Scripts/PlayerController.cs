1381856d57a5ea15bf07bf8f264dd60a5ee84033
joanby <joan-by@hotmail.com>
2018-07-10 11:47:32 +0200

    Controlador y animaciones

---
+++ DefendTheHive/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public string idleState, walkState, runState, jumpState, throwState, dieState;
+    bool isWalking, isRunning, isJumping, isIdle, isDead, forward, backward, left, right;
+    Animator m_Animator;
+
+	// Use this for initialization
+	void Start () {
+        m_Animator = GetComponent<Animator>();
+        isIdle = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// DOWN STATES
+        if(Input.GetKeyDown(KeyCode.W)){
+            if(!isRunning){
+                isWalking = true;
+                isIdle = false;
+                forward = true;
+                m_Animator.SetBool(walkState, true);
+                m_Animator.SetBool(idleState, false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (!isRunning)
+            {
+                isWalking = true;
+                isIdle = false;
+                left = true;
+                m_Animator.SetBool(walkState, true);
+                m_Animator.SetBool(idleState, false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (!isRunning)
+            {
+                isWalking = true;
+                isIdle = false;
+                backward = true;
+                m_Animator.SetBool(walkState, true);
+                m_Animator.SetBool(idleState, false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (!isRunning)
+            {
+                isWalking = true;
+                isIdle = false;
+                right = true;
+                m_Animator.SetBool(walkState, true);
+                m_Animator.SetBool(idleState, false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if(isWalking){
+                isRunning = true;
+                m_Animator.SetBool(runState, true);
+                m_Animator.SetBool(walkState, false);
+            }
+        }
+
+
+        //ACTIONS
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)||Input.GetMouseButtonDown(0))
+        {
+            Throw();
+        }
+
+
+        //ACTION UP
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            forward = false;
+            if(!left && !backward && !right){
+                StopMotion();
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            left = false;
+            if (!forward && !backward && !right)
+            {
+                StopMotion();
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            backward = false;
+            if (!left && !forward && !right)
+            {
+                StopMotion();
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            right = false;
+            if (!left && !backward && !forward)
+            {
+                StopMotion();
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            if(isRunning && isWalking){
+                isRunning = false;
+                m_Animator.SetBool(walkState, true);
+                m_Animator.SetBool(runState, false);
+            }
+        }
+	}
+
+    void StopMotion(){
+        isWalking = false;
+        isRunning = false;
+        isIdle = true;
+        m_Animator.SetBool(idleState, true);
+        m_Animator.SetBool(walkState, false);
+        m_Animator.SetBool(runState, false);
+    }
+
+    void Jump(){
+        Debug.Log("Animacion de Salto");
+        m_Animator.SetBool(jumpState, true);
+        m_Animator.SetBool(idleState, false);
+        m_Animator.SetBool(walkState, false);
+        m_Animator.SetBool(runState, false);
+        StartCoroutine(ConsumeJump());
+    }
+
+    void Throw(){
+        m_Animator.SetBool(throwState, true);
+        m_Animator.SetBool(idleState, false);
+        m_Animator.SetBool(walkState, false);
+        m_Animator.SetBool(runState, false);
+        StartCoroutine(ConsumeThrow());
+    }
+
+    IEnumerator ConsumeJump(){
+        yield return new WaitForSeconds(0.66f);
+        m_Animator.SetBool(jumpState, false);
+        ReturnToMoveState();
+    }
+
+    IEnumerator ConsumeThrow(){
+        yield return new WaitForSeconds(0.66f);
+        m_Animator.SetBool(throwState, false);
+        ReturnToMoveState();
+    }
+
+    void ReturnToMoveState(){
+        if (isRunning)
+        {
+            m_Animator.SetBool(runState, true);
+        }
+        else if (isWalking)
+        {
+            m_Animator.SetBool(walkState, true);
+        }
+        else if (isIdle)
+        {
+            m_Animator.SetBool(idleState, true);
+        }
+    }
+}
--- DefendTheHive/Assets/Scripts/PlayerMotor.cs
+++ DefendTheHive/Assets/Scripts/PlayerMotor.cs
@@ -14,8 +14,6 @@ public class PlayerMotor : MonoBehaviour {
 
     private float currentJumpForce = 0, currentMoveSpeed = 0;
 
-    private Animator m_Animator;
-
 	private void Start()
 	{
         m_Rigidbody = GetComponent<Rigidbody>();
@@ -33,7 +31,7 @@ public class PlayerMotor : MonoBehaviour {
         horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
         vertical = CrossPlatformInputManager.GetAxis("Vertical");
 
-        Debug.Log("H:" + horizontal + ", V:" + vertical);
+        //Debug.Log("H:" + horizontal + ", V:" + vertical);
 
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
             m_Rigidbody.AddForce(0, jumpForce, 0);
@@ -49,7 +47,7 @@ public class PlayerMotor : MonoBehaviour {
 
 	}
 
-    private float turnAround, forwardAmount;
+    private float turnAmount, forwardAmount;
     [SerializeField] float stationaryTurnAround = 180;
     [SerializeField] float movingTurnSpeed = 360;
 
@@ -76,19 +74,64 @@ public class PlayerMotor : MonoBehaviour {
 	}
 
 
+    [SerializeField] float groundCheckDistance = 0.1f;
 	private bool isGrounded;
+    private Vector3 groundNormal;
     //Comprueba si el personaje está en el suelo
     void CheckGroundStatus(){
-       
+
+        #if UNITY_EDITOR
+            Debug.DrawLine(transform.position + Vector3.up * 1.0f,
+                       transform.position + Vector3.down * 1.0f,
+                       Color.red);  
+        #endif
+
+
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(transform.position+Vector3.up * 0.1f, //trazamos el rayo unos 10cm más arriba de la suela del jugador...
+                           Vector3.down, out hitInfo, groundCheckDistance)){
+            isGrounded = true;
+            groundNormal = hitInfo.normal;
+        } else {
+            isGrounded = false;
+            groundNormal = Vector3.up;
+        }
+
+        //Debug.Log(groundNormal);
+
     }
 
-	private void Move(Vector3 move)
+	private void Move(Vector3 movement)
 	{
-       
+        if(movement.magnitude>1.0f){
+            movement.Normalize();//aquí ahora si que tiene longitud 1
+        }
+        movement = transform.InverseTransformDirection(movement);
+        CheckGroundStatus();
+        //modificamos el movimiento según el vector normal a la superfície sobre la que camina...
+        movement = Vector3.ProjectOnPlane(movement, groundNormal);
+        turnAmount = Mathf.Atan2(move.x, move.z);
+        forwardAmount = move.z;
+        m_Rigidbody.velocity = transform.forward * currentMoveSpeed;
+        ApplyExtraRotation();
 	}
 
     void ApplyExtraRotation(){
-        
+        float turnSpeed = Mathf.Lerp(stationaryTurnAround, movingTurnSpeed, forwardAmount);
+        //s = v * t
+        transform.Rotate(0, turnSpeed * turnAmount * Time.deltaTime, 0);
     }
 
+    private Animator m_Animator;
+    [SerializeField] float moveSpeedMultiplier = 1.0f;
+
+	private void OnAnimatorMove()
+	{
+        if(isGrounded && Time.deltaTime > 0){
+            Vector3 vel = m_Animator.deltaPosition * moveSpeedMultiplier / Time.deltaTime;
+            vel.y = m_Rigidbody.velocity.y;//para que el personaje siga con la misma velocidad de salto
+            m_Rigidbody.velocity = vel;
+        }
+	}
 }
