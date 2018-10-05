64917cccbbba541e486608d0a7a4c965288ab077
joanby <joan-by@hotmail.com>
2018-10-06 00:25:46 +0200

    Pong

---
+++ Pong/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Librería para cargar la Interfaz gráfica de usuario
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager sharedInstance = null;
+    public bool gameStarted = false;
+
+
+    public Text title;
+    public Button start;
+
+	private void Awake()
+	{
+        if(sharedInstance == null){
+            sharedInstance = this;
+        }
+	}
+
+    public void StartGame(){
+        gameStarted = true;
+        title.gameObject.SetActive(false);
+        start.gameObject.SetActive(false);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Pong/Assets/Scripts/GoalZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalZone : MonoBehaviour {
+
+    /*
+     * Este método se llama automáticamente cuando algo entra dentro del trigger...
+    */
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.gameObject.tag == "Ball")
+        {
+            Debug.Log("Alguien ha marcado gol");
+        }
+	}
+
+}
